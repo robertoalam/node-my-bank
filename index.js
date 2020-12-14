be0bfe856aff54a49991587ff7cee9ddb5224c20
scarapa <scarapa@gmail.com>
2020-12-14 19:32:40 -0300

    tratando erro e inserindo logs

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 import express from "express";
 import rotasContas from "./rotas/contas.js";
 import { promises as fs } from "fs";
+import winston from "winston";
+import cors from "cors";
 
 const { readFile , writeFile } = fs;
+function dataAtualFormatada(){
+    var data = new Date(),
+        dia  = data.getDate().toString(),
+        diaF = (dia.length == 1) ? '0'+dia : dia,
+        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
+        return anoF+mesF+diaF;
+    return diaF+"/"+mesF+"/"+anoF;
+}
 
 global.arquivoContas = "./contas/contas.json";
+var arquivoLog = dataAtualFormatada();
+global.arquivoLog = "./logs/"+arquivoLog+".log";
+
+
+
+const {combine , timestamp , label , printf } = winston.format;
+const meuFormato = printf(({ level , message , label , timestamp})=>{
+    return `${timestamp} [${label}] ${level}: ${message}`
+})
+global.logger = winston.createLogger({
+    level:"silly",
+    transports:[
+        new (winston.transports.Console)(),
+        new (winston.transports.File)({filename:global.arquivoLog }),
+    ],
+    format: combine(
+        label({ label: "my-bank-api"}),
+        timestamp(),
+        meuFormato
+    )
+})
 
 const app = express();
 app.use( express.json() );
-
+app.use(cors());
+app.use( express.static("public") );
 
 app.use("/contas",rotasContas);
 app.use("/account",rotasContas);
@@ -22,13 +56,18 @@ app.use("/account",rotasContas);
 app.listen(3000, async ()=>{
     const initialJson = {
         nextId:1,
-        accounts:[]
+        contas:[]
     }
     try {
-        await readFile(global.arquivoContas)    
+        await readFile(global.arquivoContas);
+        logger.info('API STARTED')
     } catch (error) {
-        await writeFile(global.arquivoContas , JSON.stringify(initialJson,null,2));
+        await writeFile(global.arquivoContas , JSON.stringify(initialJson,null,2)).then(()=>{
+            logger.info('ERRO AO CRIAR ARQUIVO')
+        }).catch( err=>{
+            logger.error(err)
+        });
+        
     }
     
-    console.log('API STARTED')
 });
--- rotas/contas.js
+++ rotas/contas.js
@@ -4,79 +4,152 @@ import { promises as fs } from "fs";
 const { readFile, writeFile } = fs;
 const router = express.Router();
 
-let json;
 let data;
+let dados;
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res , next) => {
     let conta = req.body;
-    try 
-    {
+    try {
         let contaNova = req.body;
-        const contas = await readFile(global.arquivoContas);
-        data = JSON.parse( contas );
+
+        if( !contaNova.name ) {
+            throw new Error("Sem Nome!")
+        }
+
+        if( contaNova.saldo == null) {
+            throw new Error("Sem Saldo!")
+        }
+
+        dados = await readFile(global.arquivoContas);
+        data = JSON.parse( dados );
         contaNova = {id:data.nextId++, ... contaNova};
-        data.accounts.push(contaNova)
+        contaNova = {
+            id:data.nextId++, 
+            nome:contaNova.nome,
+            saldo: contaNova.saldo
+        };
+        data.contas.push(contaNova)
 
         await writeFile(global.arquivoContas , JSON.stringify(data,null,2) );
         res.status(200).send(contaNova);
-
+        logger.info(`POST / conta - ${JSON.stringify(contaNova)}`);
     } catch (error) {
-        res.status(400).send({error:error.message})
+        // res.status(400).send({error:error.message})
+        next(error);
     }
-    
     res.end();
 });
 
-router.get("/",async(req,res)=>{
+router.get("/",async(req,res,next)=>{
     try {
-        const contas = JSON.parse( await readFile(global.arquivoContas) );
-        delete contas.nextId;
-        res.send(contas);
+        dados = JSON.parse( await readFile(global.arquivoContas) );
+        delete dados.nextId;
+        res.send(dados);
+        logger.info("GET / contas");
     } catch (error) {
-        res.status(400).send({error:error.message})
+        // res.status(400).send({error:error.message})
+        next(error);
     }
 });
 
-router.get("/:id",async(req,res)=>{
+router.get("/:id",async(req,res,next)=>{
     try {
-        const contas = JSON.parse( await readFile(global.arquivoContas) ); 
-        const novaConta = contas.accounts.find(
+        dados = JSON.parse( await readFile(global.arquivoContas) ); 
+        const novaConta = dados.contas.find(
             conta => conta.id === parseInt(req.params.id ) 
         ) ;
         res.send(novaConta);
+        logger.info(`GET / contas - ID ${req.params.id}`);
     } catch (error) {
-        res.status(400).send({error:error.message})
+        // res.status(400).send({error:error.message})
+        next(error);
     }
 });
 
-router.delete("/:id",async(req,res)=>{
+router.delete("/:id",async(req,res,next)=>{
     try {
-        const contas = JSON.parse( await readFile(global.arquivoContas) );
-        contas.accounts = contas.accounts.filter(
+        dados = JSON.parse( await readFile(global.arquivoContas) );
+        dados.contas = dados.contas.filter(
             conta => conta.id !== parseInt(req.params.id ) 
         ) ;
         await writeFile(global.arquivoContas , JSON.stringify(contas,null,2) );
         res.status(200).send(true);
+        logger.info(`DELETE /contas/:id - ${req.params.id}`);
     } catch (error) {
-        res.status(400).send({error:error.message})
+        // res.status(400).send({error:error.message})
+        next(error);
     }
 });
 
-router.put("/",async(req,res)=>{
+router.put("/",async(req,res,next)=>{
 
     try {
-        const conta = req.body;
-        const contas = JSON.parse( await readFile(global.arquivoContas) );
-        // DESCOBRINDO O INDICE
-        const index = contas.accounts.findIndex(a => a.id === conta.id);
 
-        contas.accounts[index] = conta;
+        if( !contaNova.name ) {
+            throw new Error("Sem Nome!")
+        }
+
+        if( contaNova.saldo > 0) {
+            throw new Error("Sem Saldo!")
+        }
+
+        const conta = req.body;
+        dados = JSON.parse( await readFile(global.arquivoContas) );
+        // DESCOBRINDO O INDICE
+        const index = dados.contas.findIndex(a => a.id === conta.id);
+
+        if(index === -1){
+            throw new Error("Registro não encontrado!")
+        }
+
+        dados.contas[index].nome = conta.nome;
+        dados.contas[index].saldo = conta.saldo;
         //REGRAVANDO
-        await writeFile(global.arquivoContas , JSON.stringify(contas,null,2) );
+        await writeFile(global.arquivoContas , JSON.stringify(dados,null,2) );
 
         res.status(200).send(conta);
+
+        logger.info(`PUT / contas - ${JSON.stringify(conta)}`);
     } catch (error) {
-        res.status(400).send({error:error.message})
+        // res.status(400).send({error:error.message})
+        next(error);
     }
+});
+
+router.patch("/novoSaldo" , async (req , res , next)=>{
+    try {
+
+        if( contaNova.saldo == null) {
+            throw new Error("Sem Saldo!")
+        }
+
+        const conta = req.body;
+
+        dados = JSON.parse( await readFile(global.arquivoContas) );
+        // DESCOBRINDO O INDICE
+        const index = dados.contas.findIndex(a => a.id === parseInt(conta.id) );
+
+        if(index === -1){
+            throw new Error("Registro não encontrado!")
+        }
+
+        dados.contas[index].saldo = conta.saldo;
+        
+        await writeFile(global.arquivoContas , JSON.stringify(dados,null,2) );
+
+        res.send(dados.contas[index]);
+
+        logger.info(`PATCH / contas/novoSaldo - ${JSON.stringify(dados.contas[index])}`);
+    } catch (error) {
+        // res.status(400).send({error:error.message})
+        next(error);
+    }
+});
+
+
+// TRATAR ERROS 
+router.use((err , req, res , next)=>{
+    logger.error(`${req.method} ${req.baseUrl} ${err.message}`)
+    res.status(400).send({error: err.message});
 });
 export default router;
